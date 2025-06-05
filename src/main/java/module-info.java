3c52740083bde73775d6af06eabe636340b24c13
François <francois.dargentolle@etu.univ-amu.fr>
2025-06-05 09:43:28 +0200

    C1 : Ajout d'un nouveau package fonctionnalités avec la première classe de mouvement pour les personnages non bot

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.bomberman_sae {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.example.bomberman_sae to javafx.fxml;
     exports com.example.bomberman_sae;
